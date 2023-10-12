48c2fd6fb656b6bd6748f0b6c1f0c0d103417174
Josiah Parry <josiah.parry@gmail.com>
2023-10-12 11:29:20 -0400

    begin revamping arc_select to better handle spatial filters

--- R/arc-select.R
+++ R/arc-select.R
@@ -2,20 +2,11 @@
 #'
 #' Give a `FeatureLayer` or `Table` object, retrieve its data as an `sf` object or `tibble` resepctively.
 #'
+#' @inheritParams source
 #' @param x an object of class `FeatureLayer` or `Table`.
 #' @param fields a character vector of the field names that you wish to be returned. By default all fields are returned.
 #' @param where a simple SQL where statement indicating which features should be selected.
 #' @param crs the spatial reference to be returned. If the CRS is different than the `FeatureLayer`'s CRS, a transformation will occur server-side. Ignored for `Table` objects.
-#' @param filter_geom an object of class `bbox`, `sfc` or `sfg` used to filter
-#'   query results based on a predicate function. If an `sfc` object is provided
-#'   it will be transformed to the layers spatial reference. If the `sfc` is
-#'   missing a CRS (or is an `sfg` object) it is assumed to be in the layers
-#'   spatial reference. If an `sfc` object has multiple features, the features
-#'   are unioned with [sf::st_union()]. If an `sfc` object has MULTIPOLYGON
-#'   geometry, the features are treated as polygonal coverage and unioned with
-#'   `is_coverage = TRUE` before being cast to POLYGON geometry with
-#'   [sf::st_cast()].
-#' @param predicate default `"intersects"`. Possible options are `"intersects"`,  `"contains"`,  `"crosses"`,  `"overlaps"`,  `"touches"`, and `"within"`.
 #' @param n_max the maximum number of features to return. By default returns every feature available. Unused at the moment.
 #' @param ... additional query parameters passed to the API. See [reference documentation](https://developers.arcgis.com/rest/services-reference/enterprise/query-feature-service-layer-.htm#GUID-BC2AD141-3386-49FB-AA09-FF341145F614) for possible arguments.
 #' @export
@@ -29,134 +20,69 @@ arc_select <- function(
     n_max = Inf,
     ...
 ) {
+  # Developer note:
+  # For this function we extract the query object and manipulate the elements
+  # inside of the query object to modify our request. We then splice those
+  # values back into `x` and send our request
+  # note that everything that goes into our quey must be the json that will
+  # be sent directly to the API request which is why we convert it to json
+  # before we use `update_params()`
 
+  # object class checking
   obj_check_layer(x)
 
-  if (!missing(filter_geom)) {
-    x <- apply_filter_geom(
-        x,
-        filter_geom = filter_geom,
-        predicate = predicate,
-        crs = crs
-      )
-  }
-
+  # extract the query object
   query <- attr(x, "query")
 
-  # handle fields and where clause if missing
-  if (missing(fields)) {
-    fields <- query[["outFields"]] %||% "*"
-  }
-
-  # if not missing fields collapse to scalar character
+  # modify fields if not missing
   if (!missing(fields) && (length(fields) > 1)) {
+    # if not missing fields collapse to scalar character
     # check if incorrect field names provided
     x_fields <- x[["fields"]][["name"]]
     nindex <- tolower(fields) %in% tolower(x_fields)
 
+    # handle the case where a field is being selected that
+    # is not one of the available fields in the feature layer
     if (any(!nindex)) {
       cli::cli_abort(
         "Field{?s} not in {.arg x}: {.var {fields[!nindex]}}"
       )
     }
-
-    fields <- paste0(fields, collapse = ",")
+    # collapse together
+    query[["outFields"]] <- paste0(fields, collapse = ",")
   }
 
-  # if where is missing set to 1=1
-  if (missing(where)) {
-    where <- query[["where"]] %||% "1=1"
-  }
-
-  # handle SR
-  if (is.na(crs)) {
-    out_sr <- NULL
-  } else {
-    out_sr <- jsonify::to_json(validate_crs(crs)[[1]], unbox = TRUE)
-  }
-
-  x <- update_params(
-      x,
-      outFields = fields,
-      where = where,
-      outSR = out_sr,
-      ...
-    )
-
-  collect_layer(x, n_max = n_max)
-}
-
-#' Apply filter_geom to x
-#'
-#' @keywords internal
-apply_filter_geom <- function(x,
-                              filter_geom = NULL,
-                              predicate = "intersects",
-                              crs = sf::st_crs(x),
-                              error_call = rlang::caller_env()) {
-  check_inherits_any(
-    filter_geom,
-    class = c("sfc", "sfg", "bbox"),
-    call = error_call
-  )
-
-  if (inherits(filter_geom, "bbox")) {
-    filter_geom <- sf::st_as_sfc(filter_geom)
-  }
-
-  if (inherits(filter_geom, "sfg")) {
-    filt_crs <- crs
-  }
-
-  # if its an sfc object it must be length one
-  if (inherits(filter_geom, "sfc")) {
-    if (length(filter_geom) > 1) {
-      filter_geom <- sf::st_union(filter_geom)
+  # handle filter geometry if not missing
+  if (!missing(filter_geom)) {
+    # determine the appropriate crs
+    # should probably be made into a utility function to impute crs
+    filt_crs <- if (is.na(sf::st_crs(filter_geom))) {
+      sf::st_crs(x)
+    } else {
+      sf::st_crs(filter_geom)
     }
+    spatial_filter <- prepare_spatial_filter(filter_geom, filt_crs, predicate)
 
-    # if the CRS is missing use the layer's CRS
-    # otherwise use the CRS of the filter_geom object
-    filt_crs <- sf::st_crs(filter_geom)
-
-    if (is.na(filt_crs)) {
-      filt_crs <- crs
-    }
-
-    # extract the sfg object which is used to write Esri json
-    filter_geom <- filter_geom[[1]]
+    # append spatial filter fields to the query
+    query <- c(query, spatial_filter)
   }
 
-  # if a multi polygon stop, must be a single polygon see
-  # related issue: https://github.com/R-ArcGIS/arcgislayers/issues/4
-  if (inherits(filter_geom, "MULTIPOLYGON")) {
-    cli::cli_inform(
-      c(
-        "!" = "{.arg filter_geom} can't have {.val MULTIPOLYGON} geometry.",
-        "i" = "Using {.fn sf::st_union} and {.fn sf::st_cast} to create a
-        coverage {.val POLYGON} for {.arg filter_geom}."
-      ),
-      call = error_call
-    )
-
-    filter_geom <- sf::st_union(filter_geom, is_coverage = TRUE)
-    filter_geom <- sf::st_cast(filter_geom, to = "POLYGON")
+  # handle SR if not missing
+  if (!is.na(crs)) {
+    query[["outSR"]] <- jsonify::to_json(validate_crs(crs)[[1]], unbox = TRUE)
   }
 
-  esri_geometry <- st_as_json(filter_geom, filt_crs)
+  # update the parameters based on our query list
+  x <- update_params(x, !!!query)
 
-  filter_params <- list(
-    geometryType = determine_esri_geo_type(filter_geom),
-    geometry = st_as_json(filter_geom),
-    spatialRel = match_spatial_rel(predicate)
-    # TODO is `inSR` needed if the CRS is specified in the geometry???
-  )
-
-  update_params(x, rlang::splice(filter_params))
+  # send the request
+  collect_layer(x, n_max = n_max, ...)
 }
 
 # This is the workhorse function that actually executes the queries
 #' @keywords internal
 collect_layer <- function(x, n_max = Inf, token = Sys.getenv("ARCGIS_TOKEN"), ..., error_call = rlang::caller_env()) {
+
   obj_check_layer(x, call = error_call)
 
   # 1. Make base request
@@ -266,10 +192,11 @@ collect_layer <- function(x, n_max = Inf, token = Sys.getenv("ARCGIS_TOKEN"), ..
   res <- do.call(rbind, res)
 
   if (is.null(res)) {
-    cli::cli_warn(
+    cli::cli_alert_info(
       "No features returned from query",
       call = error_call
     )
+
     return(data.frame())
   }
 
---
+++ R/utils-spatial-filter.R
@@ -0,0 +1,107 @@
+
+#' Prepare json for spatial filters
+#'
+#' @param filter_geom an object of class `bbox`, `sfc` or `sfg` used to filter
+#'   query results based on a predicate function. If an `sfc` object is provided
+#'   it will be transformed to the layers spatial reference. If the `sfc` is
+#'   missing a CRS (or is an `sfg` object) it is assumed to be in the layers
+#'   spatial reference. If an `sfc` object has multiple features, the features
+#'   are unioned with [sf::st_union()]. If an `sfc` object has MULTIPOLYGON
+#'   geometry, the features are treated as polygonal coverage and unioned with
+#'   `is_coverage = TRUE` before being cast to POLYGON geometry with
+#'   [sf::st_cast()].
+#' @param predicate default `"intersects"`. Possible options are `"intersects"`,  `"contains"`,  `"crosses"`,  `"overlaps"`,  `"touches"`, and `"within"`.
+#'
+#' ### Spatial Binary Predicates:
+#'
+#' - esriSpatialRelIntersects
+#' - esriSpatialRelContains
+#' - esriSpatialRelCrosses
+#' - esriSpatialRelOverlaps
+#' - esriSpatialRelTouches
+#' - esriSpatialRelWithin
+#' @export
+#' @rdname spatial_filter
+prepare_spatial_filter <- function(
+    filter_geom,
+    crs,
+    predicate
+) {
+
+  if (inherits(filter_geom, "bbox")) {
+    filter_geom <- sf::st_as_sfc(filter_geom)
+  }
+
+  if (inherits(filter_geom, "sfg")) {
+    filt_crs <- crs
+  }
+
+  # if its an sfc object it must be length one
+  if (inherits(filter_geom, "sfc")) {
+    if (length(filter_geom) > 1) {
+      filter_geom <- sf::st_union(filter_geom)
+    }
+
+    # if the CRS is missing use the layer's CRS
+    # otherwise use the CRS of the filter_geom object
+    filt_crs <- sf::st_crs(filter_geom)
+
+    if (is.na(filt_crs)) {
+      filt_crs <- crs
+    }
+
+    # extract the sfg object which is used to write Esri json
+    filter_geom <- filter_geom[[1]]
+  }
+
+  # if a multi polygon stop, must be a single polygon see
+  # related issue: https://github.com/R-ArcGIS/arcgislayers/issues/4
+  if (inherits(filter_geom, "MULTIPOLYGON")) {
+    cli::cli_inform(
+      c(
+        "!" = "{.arg filter_geom} cannot be a {.val MULTIPOLYGON} geometry.",
+        "i" = "Using {.fn sf::st_union} and {.fn sf::st_cast} to create a
+        coverage {.val POLYGON} for {.arg filter_geom}."
+      ),
+      call = rlang::caller_env()
+    )
+
+    filter_geom <- sf::st_union(filter_geom, is_coverage = TRUE)
+    filter_geom <- sf::st_cast(filter_geom, to = "POLYGON")
+  }
+
+  list(
+    geometryType = determine_esri_geo_type(filter_geom),
+    geometry = as_esri_geometry(filter_geom),
+    spatialRel = match_spatial_rel(predicate)
+    # TODO is `inSR` needed if the CRS is specified in the geometry???
+  )
+}
+
+
+#' @export
+#' @rdname spatial_filter
+match_spatial_rel <- function(predicate) {
+  # determine the spatial relationship (predicate)
+  predicate <- tolower(predicate)
+  esri_predicates <- c(
+    "esriSpatialRelIntersects",
+    "esriSpatialRelContains",
+    "esriSpatialRelCrosses",
+    # "esriSpatialRelEnvelopeIntersects",
+    #   - don't provide this, just provide bbox and intersects
+    # "esriSpatialRelIndexIntersects", idk what this is remove
+    "esriSpatialRelOverlaps",
+    "esriSpatialRelTouches",
+    "esriSpatialRelWithin"
+  )
+
+  pred_arg_vals <- tolower(
+    substr(esri_predicates, 15, nchar(esri_predicates))
+  )
+
+  # ensure a correct one has been chosen
+  rlang::arg_match(predicate, pred_arg_vals)
+
+  esri_predicates[grepl(predicate, esri_predicates, ignore.case = TRUE)]
+}
